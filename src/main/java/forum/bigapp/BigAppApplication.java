96510cea4c30045820bcefa65f83af00c741f2d5
alexeyukrainets <qaaq.com.ua@gmail.com>
2023-12-28 19:25:49 +0100

    second commit

---
+++ src/main/java/forum/bigapp/BigAppApplication.java
@@ -0,0 +1,13 @@
+package forum.bigapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BigAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/forum/bigapp/BigAppApplicationTests.java
@@ -0,0 +1,13 @@
+package forum.bigapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BigAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
