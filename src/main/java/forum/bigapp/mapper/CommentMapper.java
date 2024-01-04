343862673f9bc4757e894f48cc09f7783373780f
codered23 <oleg2003andriy@gmail.com>
2024-01-04 20:30:32 +0200

    testMapper commit

--- src/main/java/forum/bigapp/mapper/CommentMapper.java
+++ src/main/java/forum/bigapp/mapper/CommentMapper.java
@@ -3,11 +3,26 @@ package forum.bigapp.mapper;
 import forum.bigapp.dto.request.CommentRequestDto;
 import forum.bigapp.dto.response.CommentResponseDto;
 import forum.bigapp.model.Comment;
-import org.mapstruct.Mapper;
-import org.mapstruct.MapperConfig;
+import forum.bigapp.model.Reply;
+import org.mapstruct.*;
+
+import java.util.List;
 
 @Mapper(config = MapperConfig.class)
 public interface CommentMapper {
+    @Mapping(source = "owner.id", target = "ownerId")
+    @Mapping(source = "topic.id", target = "topicId")
+    @Mapping(target = "repliesId", ignore = true)
     CommentResponseDto toDto(Comment comment);
+
+    @AfterMapping
+    default void setRepliesId(@MappingTarget CommentResponseDto dto, Comment comment) {
+        List<Long> repliesId = comment.getReplies()
+                .stream()
+                .map(Reply::getId)
+                .toList();
+        dto.setRepliesId(repliesId);
+    }
+
     Comment toModel(CommentRequestDto dto);
 }
