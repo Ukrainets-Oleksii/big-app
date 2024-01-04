53c566e654faa5a4e9a4d1d7be911e9aa528ae8b
codered23 <oleg2003andriy@gmail.com>
2024-01-04 21:45:23 +0200

    topicMapper completed

--- src/main/java/forum/bigapp/mapper/TopicMapper.java
+++ src/main/java/forum/bigapp/mapper/TopicMapper.java
@@ -1,18 +1,43 @@
 package forum.bigapp.mapper;
 
 import forum.bigapp.dto.request.TopicRequestDto;
+import forum.bigapp.dto.response.CommentResponseDto;
 import forum.bigapp.dto.response.TopicResponseDto;
+import forum.bigapp.model.Comment;
+import forum.bigapp.model.Reply;
 import forum.bigapp.model.Topic;
-import org.mapstruct.Mapper;
-import org.mapstruct.MapperConfig;
-import org.mapstruct.Named;
+import org.mapstruct.*;
+
+import java.util.List;
 import java.util.Optional;
 
 @Mapper(config = MapperConfig.class)
 public interface TopicMapper {
-
+    @Mapping(source = "owner.id", target = "ownerId")
+    @Mapping(target = "commentsId", ignore = true)
     TopicResponseDto toDto(Topic topic);
+
+    @AfterMapping
+    default void setCommentsId(@MappingTarget TopicResponseDto dto, Topic topic) {
+        List<Long> commentId = topic.getComments()
+                .stream()
+                .map(Comment::getId)
+                .toList();
+        dto.setCommentsId(commentId);
+    }
+
+    @Mapping(source = "ownerId", target = "owner", qualifiedByName = "userById")
+    @Mapping(target = "comments", ignore = true)
     Topic toModel(TopicRequestDto dto);
+
+    @AfterMapping
+    default void setComments(@MappingTarget Topic topic, TopicRequestDto dto) {
+        List<Comment> commentId = dto.getCommentsId()
+                .stream()
+                .map(Comment::new)
+                .toList();
+        topic.setComments(commentId);
+    }
 
     @Named("topicById")
     default Topic userById(Long id) {
