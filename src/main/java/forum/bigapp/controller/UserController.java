4e7e63810f42f836f2bc6884240cf67a65750f6d
alexeyukrainets <qaaq.com.ua@gmail.com>
2024-01-07 20:02:42 +0100

    crested UserController

---
+++ src/main/java/forum/bigapp/controller/UserController.java
@@ -0,0 +1,52 @@
+package forum.bigapp.controller;
+
+import forum.bigapp.dto.request.UserRequestDto;
+import forum.bigapp.dto.response.UserResponseDto;
+import forum.bigapp.mapper.UserMapper;
+import forum.bigapp.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+
+@RequiredArgsConstructor
+@RequestMapping("/users")
+@RestController
+public class UserController {
+    private final UserService service;
+    private final UserMapper mapper;
+
+    @PostMapping("/create")
+    public UserResponseDto create(@RequestBody UserRequestDto dto) {
+        return mapper.toDto(service.save(mapper.toModel(dto)));
+    }
+
+    @GetMapping("/{id}")
+    public UserResponseDto findById(@PathVariable Long id) {
+        return mapper.toDto(service.getByID(id));
+    }
+
+    @GetMapping("/findAll")
+    public List<UserResponseDto> findAll() {
+        return service.findAll().stream()
+                .map(mapper::toDto)
+                .toList();
+    }
+
+    @PutMapping("/{id}")
+    public UserResponseDto update(@PathVariable Long id,
+                                  @RequestBody UserRequestDto dto) {
+        return mapper.toDto(service.update(id, mapper.toModel(dto)));
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        service.deleteById(id);
+    }
+}
