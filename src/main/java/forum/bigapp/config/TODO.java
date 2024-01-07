950c2a2db74f031e901aa40ea174f8d17c83de2f
alexeyukrainets <qaaq.com.ua@gmail.com>
2024-01-07 21:11:29 +0100

    add TODO class

---
+++ src/main/java/forum/bigapp/config/TODO.java
@@ -0,0 +1,14 @@
+package forum.bigapp.config;
+
+public class TODO {
+    //TODO Exception Handler
+    //TODO Spring Security
+    //TODO Registration and Authentications
+    //TODO Create JWT tokens
+    //TODO testing
+    //TODO Liquibase
+    //TODO Docker
+    //TODO dile README!!!!!
+    //TODO Pagination and sorting
+    //TODO Front-end
+}
