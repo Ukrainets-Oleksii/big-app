a4e2437439b82eecea215294f9df674c4009e05e
codered23 <oleg2003andriy@gmail.com>
2024-01-08 21:24:58 +0200

    added CustomGlobalExceptionHandler

---
+++ src/main/java/forum/bigapp/exception/EntityNotFoundException.java
@@ -0,0 +1,11 @@
+package forum.bigapp.exception;
+
+public class EntityNotFoundException extends RuntimeException {
+    public EntityNotFoundException(String message) {
+        super(message);
+    }
+
+    public EntityNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
---
+++ src/main/java/forum/bigapp/exception/RegistrationException.java
@@ -0,0 +1,11 @@
+package forum.bigapp.exception;
+
+public class RegistrationException extends RuntimeException {
+    public RegistrationException(String message) {
+        super(message);
+    }
+
+    public RegistrationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
