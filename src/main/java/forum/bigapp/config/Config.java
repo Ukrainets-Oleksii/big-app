da6bbdf2f01716e7289e37ac9244c80cf2446593
codered23 <oleg2003andriy@gmail.com>
2024-01-29 20:27:58 +0200

    small changes

--- src/main/java/forum/bigapp/config/Config.java
+++ src/main/java/forum/bigapp/config/Config.java
@@ -1,13 +1,8 @@
 package forum.bigapp.config;
 
-import org.springframework.context.annotation.Configuration;
 import java.time.format.DateTimeFormatter;
 
-@Configuration
 public class Config {
     public static final DateTimeFormatter format
             = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-    public static final String PATTERN_OF_EMAIL
-            = "^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)"
-            + "*@[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$";
 }
